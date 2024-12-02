cd1a0fc0b36a4bc03aa681149392a21eb7a756af
VadimRevega <vadim.revega@gmail.com>
2024-12-02 07:21:48 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,19 @@
+# 1Задача:
+# first = ['Strings', 'Student', 'Computers']
+# second = ['Строка', 'Урбан', 'Компьютер']
+# Необходимо создать 2 генераторных сборки:
+
+first = ['Strings', 'Student', 'Computers']
+# 2
+second = ['Строка', 'Урбан', 'Компьютер']
+
+# Списковые словарные сборки которыее:
+# высчитывает разницу длин строк из списков first и second
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+
+# содержит результаты сравнения длин строк в одинаковых позициях из списков first и second
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+# Вывод результата
+print(list(first_result))
+print(list(second_result))
